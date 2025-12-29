ba6e659bec34ea007b57ff8b2e1e959effab4f5b
blakecline <blakecline78@gmail.com>
2025-12-28 20:20:14 -0600

    Add files via upload

---
+++ Module 1 Critical Thinking.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "First Name: Jake" << endl;
+    cout << "Last Name: Spectre"  << endl;
+    cout << "Street Address: 123 Main Street" << endl;
+    cout << "City: Stillwater" << endl;
+    cout << "Zip Code: 74075" << endl;
+
+    return 0;
+}
