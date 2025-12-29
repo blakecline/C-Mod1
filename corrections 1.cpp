acf7d34fac107dbd3bf8c9e4ed0dce4cf31093ef
blakecline <blakecline78@gmail.com>
2025-12-28 20:29:25 -0600

    Update corrections 1.cpp

--- corrections 1.cpp
+++ corrections 1.cpp
@@ -4,12 +4,9 @@ using namespace std;
 // Main Function
 int main()
 {
-    double myMoney = 1000.50; // this should be printed out
-
-    // Standard Output Statements
-    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
     cout << "I have corrected all errors for this program." << endl;
-    cout << "The total amount of money available is = " << myMoney << endl;
 
     // Main Function return Statement
     return 0;
